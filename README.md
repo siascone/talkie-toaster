de17290f37b0f1225fbc72c1b5e0bc792b0e0ea2
siascone <spencer.iascone@gmail.com>
2022-06-23 13:58:50 -0600

    Setup app, static_pages#root, react

---
+++ README.md
@@ -0,0 +1,3 @@
+# Talkie Toaster
+
+Talkie's the name, toasting's the game... would you like some toast?
---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div id='app'>
+            This is React Land
+        </div>
+    )
+}
+
+export default App;
